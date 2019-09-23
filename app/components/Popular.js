81a669df0ee9f3d904b93905c566645460b223cb
DanielGonzalez <jodgonzalezvi@unal.edu.co>
2019-09-23 16:49:46 -0500

    d

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,15 @@
 import React from 'react'
 
 export default class Popular extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            selectedLanguage: 'All'
+        }
+    }
+    updateSelectedLanguage(selectedLanguage){
+        this.setState({selectedLanguage})
+    }
     render(){
         const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python', 'Golang']    
 
@@ -8,7 +17,9 @@ export default class Popular extends React.Component {
             <ul className='flex-center'>
                 {languages.map((language) => (
                 <il key={language}>
-                    <button className='btn-clear nav-link'>
+                    <button className='btn-clear nav-link'
+                    style = {language === this.state.selectedLanguage ? {color: 'rgb(187, 46, 31)'} : null}
+                    onClick={()=>{this.updateSelectedLanguage(language)}}>
                         {language}
                     </button>
                 </il>
